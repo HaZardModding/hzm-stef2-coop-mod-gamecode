a787b50fcb27d1c4ff6c9f7c49ec2b88d327dba2
chrissstrahl <Chrissstrahl@users.noreply.github.com>
2022-09-19 00:53:53 +0200

    Fixed Objectives not showing if they have been set/defined before the 1 sec on the level time has passed

--- code/dlls/game/coopPlayer.hpp
+++ code/dlls/game/coopPlayer.hpp
@@ -98,7 +98,7 @@ public:
 	//hzm coop mod chrissstrahl - used to saveoff the player his last class info update time
 	float	lastTimeUpdatedClassStat = -999.0f;
 	//hzm coop mod chrissstrahl - used to save when the player has recived the last time the mission objectives
-	float	lastTimeUpdatedObjectives = 0.0f;
+	float	lastTimeUpdatedObjectives = -1.0f;
 	//hzm coop mod chrissstrahl - used to save when the player was used the last time by another player
 	float	lastTimeUsedClassMsg = 0.0f;
 	//hzm coop mod chrissstrahl - used to determin when player was injured last time
